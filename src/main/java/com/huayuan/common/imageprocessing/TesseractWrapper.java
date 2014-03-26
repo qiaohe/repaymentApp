7bdf669b9f39b7816bb2f1d49d81f62752d24240
tusc_heqiao@163.com <heqiao75518?>
2014-03-26 10:19:02 +0800

    1) update wrapper class for this project

--- src/main/java/com/huayuan/common/imageprocessing/TesseractWrapper.java
+++ src/main/java/com/huayuan/common/imageprocessing/TesseractWrapper.java
@@ -7,7 +7,6 @@ public final class TesseractWrapper {
     private static final String TESSERACT_PATH = "";
     private static final String TESSERACT_COMMAND_PATTERN = "%s %s %s -l chi_sim -psd 7 nobatch";
 
-
     public String getTesseractCommand(String sourceFileName, String outputFileName) {
         return String.format(TESSERACT_COMMAND_PATTERN, TESSERACT_PATH, sourceFileName, outputFileName);
     }
---
+++ src/main/java/com/huayuan/service/ApplicationService.java
@@ -0,0 +1,7 @@
+package com.huayuan.service;
+
+/**
+ * Created by dell on 14-3-25.
+ */
+public interface ApplicationService {
+}
