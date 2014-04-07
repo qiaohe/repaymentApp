7b2a23296371489e4afacdb1af6b925430f8bdd6
Thomas-Peng <debianthomas@gmail.com>
2014-04-07 18:08:11 +0800

    image upload in limit page
    the events triggered to upload id card photos and http requests to the
    ocr url

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,5 +1,20 @@
 $(function(){
+// image upload control
+$('#id-card-front').after($('<div><input type="file"  name="id-card-front" accept="image/*" id="id-card-front-upload" capture></div>').css('display', 'none'));
 $('#id-card-front').click(function(){
-	
+	$('#id-card-front-upload').trigger('click');
 });
+$('#id-card-front-upload').change(function(e){
+	var reader = new FileReader();
+	reader.readAsDataURL(e.target.files[0]);
+	reader.onload = function(e){
+		$.ajax({
+			url: "192.168.0.186:8080/repaymentApp/api/dict/binCode",
+			data: {
+				id-card-front: 
+			},
+		});
+	};
+});
+//
 });
