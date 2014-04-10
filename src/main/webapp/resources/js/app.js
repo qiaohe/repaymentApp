1869e14c64c8d5d8be98240864354f5b9bb30f00
Thomas-Peng <debianthomas@gmail.com>
2014-04-10 09:53:17 +0800

    modified html and css
    change the layout and the style of pages

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,4 +1,6 @@
 $(function(){
+// set the size of front-cover
+	$('#front-cover').css('height', 0.35 * $(window).height());
 // image upload
 	// id-card front
     $('#id-card-front').after($('<div><input type="file"  name="id-card-front" accept="image/*" id="id-card-front-upload" capture></div>').css('display', 'none'));
@@ -66,18 +68,21 @@ $(function(){
 	});
 // limit step 2
 	//industry
-	$('#industry').click(function(){
-		$.get("http://localhost:8080/repayment/api/dict/industry", function(json){
-			var $industry = $('<div data-role="main" class="ui-content"><select name="industry" id="industry-select"></select>xxx</div>');
-			$industry.popup();
-			$industry.popup('open');
-			console.log(json);
-			// for(var i in json){
-				// $('#industry-select').append('<option value=' + json + '></option>');
-			// }
-			
+		// $.get("http://192.168.0.191:8080/repayment/api/dict/industry", function(json){
+			var json = [{"key":"1","value":"政府机关、社会团体"},{"key":"2","value":"军事、公检法"},{"key":"3","value":"学校、医院"},{"key":"4","value":"专业事务所"},{"key":"5","value":"信息通信、IT互联网"},{"key":"6","value":"金融业"},{"key":"7","value":"交通运输"},{"key":"8","value":"公共事业"},{"key":"9","value":"能源矿产"},{"key":"10","value":"商业零售、内外贸易"},{"key":"11","value":"房地产、建筑业"},{"key":"12","value":"加工、制造业"},{"key":"13","value":"餐饮、酒店、旅游"},{"key":"14","value":"服务、咨询"},{"key":"15","value":"媒体、体育、娱乐"},{"key":"16","value":"农林牧渔"},{"key":"17","value":"网店店主"},{"key":"18","value":"学生"},{"key":"19","value":"自由职业者"},{"key":"20","value":"其他"}];
+			for(var i in json){
+				$('#industry-select').append('<option value=' + json[i].key + '>' + json[i].value + '</option>');
+			}
 			// json.key
 			// json.value
-		});
-	});
+		// });
+	//education
+		// $.get("http://192.168.0.191:8080/repayment/api/dict/industry", function(json){
+			var json_1 = [{"key":"1","value":"初中及以下"},{"key":"2","value":"高中、中专"},{"key":"3","value":"大专"},{"key":"4","value":"本科"},{"key":"5","value":"硕士"},{"key":"6","value":"博士"}];
+			for(var i in json_1){
+				$('#education-select').append('<option value=' + json_1[i].key + '>' + json_1[i].value + '</option>');
+			}
+			// json.key
+			// json.value
+		// });
 });
