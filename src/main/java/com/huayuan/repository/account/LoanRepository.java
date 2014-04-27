2fd92ff58013e922f52eeda5272d313bd86c17e7
tusc_heqiao <tusc_heqiao@163.com>
2014-04-27 09:56:11 +0800

    1)Update post update process in loan application.

--- src/main/java/com/huayuan/repository/account/LoanRepository.java
+++ src/main/java/com/huayuan/repository/account/LoanRepository.java
@@ -3,8 +3,11 @@ package com.huayuan.repository.account;
 import com.huayuan.domain.accounting.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Created by dell on 14-4-8.
  */
 public interface LoanRepository extends JpaRepository<Loan, Long> {
+    public List<Loan> findByMemberId(Long memberId);
 }
--- src/main/java/com/huayuan/repository/account/PayRepository.java
+++ src/main/java/com/huayuan/repository/account/PayRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * Created by dell on 14-4-24.
  */
-public interface TransferRepository  extends JpaRepository<Pay, Long> {
+public interface PayRepository extends JpaRepository<Pay, Long> {
 }
