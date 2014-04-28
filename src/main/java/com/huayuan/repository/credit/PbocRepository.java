72c9f7ffb303a1cd293d85b875c1d63d4f2be380
tusc_heqiao@163.com <heqiao75518?>
2014-04-28 19:16:22 +0800

    change configration file

---
+++ src/main/java/com/huayuan/repository/credit/PbocRepository.java
@@ -0,0 +1,10 @@
+package com.huayuan.repository.credit;
+
+import com.huayuan.domain.credit.Pboc;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by dell on 14-4-28.
+ */
+public interface PbocRepository extends CrudRepository<Pboc, Long>, PbocRepositoryCustom {
+}
