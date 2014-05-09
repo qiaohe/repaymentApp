bae732ba59da2b1d3ed6e2ab816768ec7b17ab00
tusc_heqiao@163.com <heqiao75518?>
2014-05-09 22:21:39 +0800

    update databse script for realease 0.0.2

--- src/main/java/com/huayuan/domain/credit/ApplicationSummary.java
+++ src/main/java/com/huayuan/domain/credit/ApplicationSummary.java
@@ -10,12 +10,25 @@ public class ApplicationSummary {
     private String name;
     private String idCardNo;
     private Integer existingFlag;
-    private Date applyDate;
+    private String applyDate;
     private String mobileCity;
     private Integer status;
-    private Date createDate;
+    private String createDate;
+    private String creditor;
 
     public ApplicationSummary() {
+    }
+
+    public ApplicationSummary(String appNo, String name, String idCardNo, Integer existingFlag, String applyDate, String mobileCity, Integer status, String createDate, String creditor) {
+        this.applyDate = applyDate;
+        this.appNo = appNo;
+        this.status = status;
+        this.existingFlag = existingFlag;
+        this.idCardNo = idCardNo;
+        this.name = name;
+        this.mobileCity = mobileCity;
+        this.createDate = createDate;
+        this.creditor = creditor;
     }
 
 
@@ -51,11 +64,11 @@ public class ApplicationSummary {
         this.existingFlag = existingFlag;
     }
 
-    public Date getApplyDate() {
+    public String getApplyDate() {
         return applyDate;
     }
 
-    public void setApplyDate(Date applyDate) {
+    public void setApplyDate(String applyDate) {
         this.applyDate = applyDate;
     }
 
@@ -75,11 +88,19 @@ public class ApplicationSummary {
         this.status = status;
     }
 
-    public Date getCreateDate() {
+    public String getCreateDate() {
         return createDate;
     }
 
-    public void setCreateDate(Date createDate) {
+    public void setCreateDate(String createDate) {
         this.createDate = createDate;
+    }
+
+    public String getCreditor() {
+        return creditor;
+    }
+
+    public void setCreditor(String creditor) {
+        this.creditor = creditor;
     }
 }
--- src/main/java/com/huayuan/repository/applicationloan/ApplicationRepositoryImpl.java
+++ src/main/java/com/huayuan/repository/applicationloan/ApplicationRepositoryImpl.java
@@ -7,6 +7,8 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,6 +18,19 @@ import java.util.List;
 public class ApplicationRepositoryImpl implements ApplicationRepositoryCustom {
     @PersistenceContext
     private EntityManager em;
+
+    public static <T> T map(Class<T> type, Object[] tuple) {
+        List<Class<?>> tupleTypes = new ArrayList<>();
+        for (Object field : tuple) {
+            tupleTypes.add(field.getClass());
+        }
+        try {
+            Constructor<T> ctor = type.getConstructor(tupleTypes.toArray(new Class<?>[tuple.length]));
+            return ctor.newInstance(tuple);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public void execute(Application application) {
@@ -27,15 +42,15 @@ public class ApplicationRepositoryImpl implements ApplicationRepositoryCustom {
     @Override
     @SuppressWarnings("unchecked")
     public List<ApplicationSummary> findApplicationSummaries() {
-        return em.createNamedQuery("Application.findApplicationSummaries").getResultList();
+        List<ApplicationSummary> result = new ArrayList<>();
+        for (Object item : em.createNamedQuery("Application.findApplicationSummaries").getResultList()) {
+            result.add(map(ApplicationSummary.class, (Object[]) item));
+        }
+        return result;
     }
 
     public Object findApplicationBy(final String appNo) {
-        final String query = "select appl.*, tv.DECISION, tv.TV_MEM_ANS_1, tv.TV_MEM_ANS_2, tv.TV_QUES_1, tv.TV_QUES_2, tv.TYPE, aval.CLASS, aval.DECISION as approvedDecision, aval.OPINION,aval.PROFILE, aval.REASON_1, aval.REASON_2, aval.REASON_3, aval.REPAY_TYPE as approvedRepayType, aval.SUG_CRL,\n" +
-                "score.PBOC_BACK_TIME,score.PBOC_TIME,score.RATING, score.RISK_REMIND,score.SCORE,member.BLOCK_CODE, member.EDUCATION,member.INDUSTRY,member.MOBILE,member.NAME, member.ORG_FLAG, member.POINTS,member.PRE_CRL, member.STATUS as memberStatus,member.PRE_RATING as memberPreRating,member.PRE_SCORE as memberPreScore\n" +
-                "from APPL appl, APPL_TV tv, APPROVAL aval, A_SCORE score, MEMBER member, ID_CARD idcard\n" +
-                " where appl.APPL_NO = tv.APPL_NO and appl.APPL_NO = aval.APPL_NO and appl.APPL_NO = score.APPL_NO and appl.MEMBER_ID = member.ID and member.ID = idcard.MEMBER_ID and appl.APPL_NO = ?";
-        return em.createNativeQuery(query).setParameter(1, appNo).getSingleResult();
+        return null;
     }
 
 }
