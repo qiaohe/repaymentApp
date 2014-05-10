83950e65f679ab16169d113c32dc36c47b43ffb3
tusc_heqiao@163.com <heqiao75518?>
2014-05-10 16:16:51 +0800

    add missed files for telephone tv features in credit system.

---
+++ src/main/java/com/huayuan/repository/applicationloan/TelephoneTvRepository.java
@@ -0,0 +1,13 @@
+package com.huayuan.repository.applicationloan;
+
+import com.huayuan.domain.loanapplication.TelephoneTV;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Created by dell on 14-5-10.
+ */
+public interface TelephoneTVRepository extends JpaRepository<TelephoneTV, Long> {
+    public List<TelephoneTV> findByApplication_ApplicationNo(String appNo);
+}
