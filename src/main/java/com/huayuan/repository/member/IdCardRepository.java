e09b4dd0d83d59c80e54d9e754b2b1d6f18977a4
tusc_heqiao@163.com <heqiao75518?>
2014-05-13 16:28:23 +0800

    add missed files for telephone tv features in credit system.

--- src/main/java/com/huayuan/repository/member/IdCardRepository.java
+++ src/main/java/com/huayuan/repository/member/IdCardRepository.java
@@ -4,10 +4,11 @@ package com.huayuan.repository.member;
 import com.huayuan.domain.member.IdCard;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by Johnson He on 14-3-26.
  */
 public interface IdCardRepository extends CrudRepository<IdCard, Long>, IdCardRepositoryCustom {
-    public IdCard findByIdNo(final String id_No);
-
+    public List<IdCard> findByIdNo(String id_No);
 }
