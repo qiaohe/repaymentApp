e53f6ce1d85ecc61356c6d8db1a5c4528e4aeb72
tusc_heqiao@163.com <heqiao75518?>
2014-05-14 13:50:41 +0800

    add missed files for telephone tv features in credit system.

--- src/main/java/com/huayuan/domain/idgenerator/IdSequenceGenerator.java
+++ src/main/java/com/huayuan/domain/idgenerator/IdSequenceGenerator.java
@@ -4,6 +4,7 @@ import com.huayuan.repository.IdSequenceRepository;
 import org.apache.commons.lang.StringUtils;
 import org.joda.time.DateTime;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
 import java.text.MessageFormat;
@@ -13,6 +14,7 @@ import java.util.concurrent.ConcurrentHashMap;
  * Created by Johnson on 4/4/14.
  */
 @Component(value = "idSequenceGenerator")
+@Transactional
 public class IdSequenceGenerator {
     public static final long NOT_FOUND = 0;
     private static final long STEP = 100;
