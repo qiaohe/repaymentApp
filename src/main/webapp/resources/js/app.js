5bdf5e3a67424999d005d9561d029698c7233352
Thomas-Peng <debianthomas@gmail.com>
2014-05-30 19:27:47 +0800

    add control of limit test with another card,

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -714,9 +714,11 @@ $(document).on("pagebeforeshow", "#result", function(){
         $("#option-1").attr("href", "#fail").css("background-color", "#3ca0e6");
     }
 
-    $("#option-3").tap(function () {
-        member.anothertest = 1;
-    });
+    if (!(member.status == "5.1" || parseFloat(memebr.status) > 5.2)) {
+        $("#option-3").tap(function () {
+            member.anothertest = 1;
+        });
+    }
 });
 
 $(document).on("pagecreate", "#loan", function () {
