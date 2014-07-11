e4d67de541e9e0e330df60ed538fba9dcde23541
Richard.Xue <liming.snowy@gmail.com>
2014-07-11 19:35:06 +0800

    Fix bug about wechat's share function

--- src/main/webapp/resources/js/app2.js
+++ src/main/webapp/resources/js/app2.js
@@ -37,55 +37,51 @@ $(document).on("pagecreate", "#team", function () {
     $(".member-item-right").width(curWidth*0.9-85);
 });
 
-resetWechatShare();
+WeixinApi.ready(function(Api) {
+    // 微信分享的数据
+    var wxData = {
+        "appId": "", // 服务号可以填写appId
+        "imgUrl" : 'http://godzilla.dlinkddns.com.cn/repaymentApp/resources/img/public/logo.png',
+        "link" : window.location.origin + window.location.pathname + "#prom",
+        "desc" : '一直以来我都觉得没有人帮我还信用卡是不科学的，今天终于被我找到了！哈哈哈哈',
+        "title" : "终于找到了，帮我还信用卡的那个人"
+    };
 
-function resetWechatShare() {
-    if(typeof WeixinJSBridge === "undefined") {
-        return;
-    }
-    var share = {};
-    share.title = "终于找到了，帮我还信用卡的那个人";
-    share.desc = "一直以来我都觉得没有人帮我还信用卡是不科学的，今天终于被我找到了！哈哈哈哈";
-    share.img_url = "../img/public/logo.png";
-    share.link = window.location.origin + window.location.pathname.replace("index.html","index2.html") + "#prom";
-    share.appid = "";
-
-    document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-        WeixinJSBridge.on('menu:share:appmessage', function(argv){
-            WeixinJSBridge.invoke("sendAppMessage",{
-                "appid": share.appid,
-                "img_url": share.img_url,
-                "img_width": "200",
-                "img_height": "150",
-                "link": share.link,
-                "desc": share.desc,
-                "title": share.title
-            }, function(res) {
-                console.log(res);
-            });
-        });
-        WeixinJSBridge.on('menu:share:timeline', function(argv){
-            WeixinJSBridge.invoke("shareTimeline",{
-                "img_url": share.img_url,
-                "img_width": "200",
-                "img_height": "150",
-                "link": share.link,
-                "desc": share.desc,
-                "title": share.title
-            }, function(res) {
-                console.log(res);
-            });
-        });
-        WeixinJSBridge.on('menu:share:weibo', function(argv){
-            WeixinJSBridge.invoke('shareWeibo',{
-                "content": share.desc,
-                "url": share.link
-            }, function(res) {
-                console.log(res);
-            });
-            return false;
-        });
-    }, false);
-}
+    // 分享的回调
+    var wxCallbacks = {
+        // 分享操作开始之前
+        ready : function() {
+            // 你可以在这里对分享的数据进行重组
+            console.log("准备分享");
+        },
+        // 分享被用户自动取消
+        cancel : function(resp) {
+            // 你可以在你的页面上给用户一个小Tip，为什么要取消呢？
+            console.log("分享被取消");
+        },
+        // 分享失败了
+        fail : function(resp) {
+            // 分享失败了，是不是可以告诉用户：不要紧，可能是网络问题，一会儿再试试？
+            console.log("分享失败");
+        },
+        // 分享成功
+        confirm : function(resp) {
+            // 分享成功了，我们是不是可以做一些分享统计呢？
+            //window.location.href='http://192.168.1.128:8080/wwyj/test.html';
+            console.log("分享成功");
+        },
+        // 整个分享过程结束
+        all : function(resp) {
+            // 如果你做的是一个鼓励用户进行分享的产品，在这里是不是可以给用户一些反馈了？
+            console.log("分享结束");
+        }
+    };
+    // 用户点开右上角popup菜单后，点击分享给好友，会执行下面这个代码
+    Api.shareToFriend(wxData, wxCallbacks);
+    // 点击分享到朋友圈，会执行下面这个代码
+    Api.shareToTimeline(wxData, wxCallbacks);
+    // 点击分享到腾讯微博，会执行下面这个代码
+    Api.shareToWeibo(wxData, wxCallbacks);
+});
 
 console.log("END!");
