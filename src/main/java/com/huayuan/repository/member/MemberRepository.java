b1de045f3f38082a56370b5007101659c509a632
tusc_heqiao@163.com <heqiao75518?>
2014-07-11 16:51:12 +0800

    validate the mobilephone and email whether used by another.

--- src/main/java/com/huayuan/repository/member/MemberRepository.java
+++ src/main/java/com/huayuan/repository/member/MemberRepository.java
@@ -1,15 +1,17 @@
 package com.huayuan.repository.member;
 
 import com.huayuan.domain.member.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by dell on 14-3-19.
  */
-public interface MemberRepository extends CrudRepository<Member, Long>,MemberRepositoryCustom {
+public interface MemberRepository extends CrudRepository<Member, Long>, MemberRepositoryCustom {
     public Member findByWcNo(String weChatNo);
 
     public Member findByIdCard_IdNo(String idCardNo);
 
+    public Member findByMobile(String mobilePhone);
+
+    public Member findByEmail(String email);
 }
