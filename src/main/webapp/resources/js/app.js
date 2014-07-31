d7d571714ff53254591bafcc9bee9d5f3cd21032
Richard.Xue <liming.snowy@gmail.com>
2014-07-31 11:00:54 +0800

    Resolve the flash on repayment page

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1530,6 +1530,7 @@ function generateCarousels(loanSummary) {
         contentHtml += "<div class='repayment-item'>" + generateItemLoan(loan,i) + "</div>";
     }
     $("#container").html(contentHtml);
+    $("#repayFooterDiv").show();
 
     member.crntCaro = 0;
     sliderPage();
@@ -1554,7 +1555,8 @@ function generateCarousels(loanSummary) {
             detailInfo.push("每月" + perDay + "日");
         }
         detailInfo.push(dict.numberWithCommas(curLoan.dueAmt.toFixed(2)));
-        detailInfo.push(dict.numberWithCommas(curLoan.lastDueAmt.toFixed(2)));
+        detailInfo.push(dict.numberWithCommas(Math.abs(curLoan.lastDueAmt).toFixed(2)));
+
 
         var $aim = $("#loan-specific");
         $("#loan-specific").find(".loan-d-r").each(function(i,item){
