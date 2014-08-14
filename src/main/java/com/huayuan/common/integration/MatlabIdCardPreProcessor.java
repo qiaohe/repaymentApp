ce2b2730601fd92e4d0cb1549e5e8ceba087ed67
tusc_heqiao@163.com <heqiao75518?>
2014-08-14 16:40:35 +0800

    Crawle the bill from email server using javamail library

--- src/main/java/com/huayuan/common/integration/MatlabIdCardPreProcessor.java
+++ src/main/java/com/huayuan/common/integration/MatlabIdCardPreProcessor.java
@@ -1,49 +1,49 @@
 package com.huayuan.common.integration;
 
-import com.mathworks.toolbox.javabuilder.MWCharArray;
-import com.mathworks.toolbox.javabuilder.MWException;
-import processor.ImageProcessor;
+//import com.mathworks.toolbox.javabuilder.MWCharArray;
+//import com.mathworks.toolbox.javabuilder.MWException;
+//import processor.ImageProcessor;
 
 /**
  * Created by Administrator on 14-6-27.
  */
 public class MatlabIdCardPreProcessor {
-    private static final ImageProcessor processor;
-
-    static {
-        try {
-            processor = new ImageProcessor();
-        } catch (MWException e) {
-            throw new IllegalStateException("Can not init MatLab runtime");
-        }
-    }
-
-    public boolean batchPreProcessingImage(String sourceDir, String destinationDir) {
-        MWCharArray source = new MWCharArray(sourceDir);
-        MWCharArray destination = new MWCharArray(destinationDir);
-        try {
-            Object[] result = processor.batchPreProcessingImage(1, source, destination);
-            return result != null && result.length > 0;
-        } catch (MWException e) {
-            throw new IllegalStateException("Can not init MatLab function batchPreProcessingImage");
-        }
-    }
-
-    public boolean preProcessingImage(String fileName, String destinationDir) {
-        MWCharArray fName = new MWCharArray(fileName);
-        MWCharArray destination = new MWCharArray(destinationDir);
-        try {
-            Object[] result = processor.preProcessingImage(1, fName, destination);
-            return result != null && result.length > 0;
-        } catch (MWException e) {
-            throw new IllegalStateException("Can not init MatLab function preProcessingImage");
-        }
-    }
-
-    public static void main(String[] args) {
-        MatlabIdCardPreProcessor matlabIdCardPreProcessor = new MatlabIdCardPreProcessor();
-        System.out.println(matlabIdCardPreProcessor.toString());
-
-    }
+//    private static final ImageProcessor processor;
+//
+//    static {
+//        try {
+//            processor = new ImageProcessor();
+//        } catch (MWException e) {
+//            throw new IllegalStateException("Can not init MatLab runtime");
+//        }
+//    }
+//
+//    public boolean batchPreProcessingImage(String sourceDir, String destinationDir) {
+//        MWCharArray source = new MWCharArray(sourceDir);
+//        MWCharArray destination = new MWCharArray(destinationDir);
+//        try {
+//            Object[] result = processor.batchPreProcessingImage(1, source, destination);
+//            return result != null && result.length > 0;
+//        } catch (MWException e) {
+//            throw new IllegalStateException("Can not init MatLab function batchPreProcessingImage");
+//        }
+//    }
+//
+//    public boolean preProcessingImage(String fileName, String destinationDir) {
+//        MWCharArray fName = new MWCharArray(fileName);
+//        MWCharArray destination = new MWCharArray(destinationDir);
+//        try {
+//            Object[] result = processor.preProcessingImage(1, fName, destination);
+//            return result != null && result.length > 0;
+//        } catch (MWException e) {
+//            throw new IllegalStateException("Can not init MatLab function preProcessingImage");
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        MatlabIdCardPreProcessor matlabIdCardPreProcessor = new MatlabIdCardPreProcessor();
+//        System.out.println(matlabIdCardPreProcessor.toString());
+//
+//    }
 
 }
