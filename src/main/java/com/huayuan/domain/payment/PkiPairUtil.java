1df915b12711eb3adebe2f13170ed8be32131570
tusc_heqiao@163.com <heqiao75518?>
2014-08-14 12:47:38 +0800

    Crawle the bill from email server using javamail library

--- src/main/java/com/huayuan/domain/payment/PkiPairUtil.java
+++ src/main/java/com/huayuan/domain/payment/PkiPairUtil.java
@@ -1,48 +1,40 @@
 package com.huayuan.domain.payment;
 
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.security.KeyStore;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.Signature;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
+public class PkiPairUtil {
+    private static final char[] KEY_PWD = "memedai".toCharArray();
 
-public class Pkipair {
     public String signMsg(String signMsg) {
-        String base64 = "";
         try {
-            KeyStore ks = KeyStore.getInstance("PKCS12");
-            ClassPathResource resource = new ClassPathResource("99bill-rsa.pfx");
-            BufferedInputStream ksbufin = new BufferedInputStream(resource.getInputStream());
-            char[] keyPwd = "memedai".toCharArray();
-            ks.load(ksbufin, keyPwd);
-            PrivateKey priK = (PrivateKey) ks.getKey("test-alias", keyPwd);
+            KeyStore keyStore = KeyStore.getInstance("PKCS12");
+            BufferedInputStream bis = new BufferedInputStream(new ClassPathResource("99bill-rsa.pfx").getInputStream());
+            keyStore.load(bis, KEY_PWD);
+            PrivateKey privateKey = (PrivateKey) ks.getKey("test-alias", KEY_PWD);
             Signature signature = Signature.getInstance("SHA1withRSA");
-            signature.initSign(priK);
+            signature.initSign(privateKey);
             signature.update(signMsg.getBytes("utf-8"));
-            sun.misc.BASE64Encoder encoder = new sun.misc.BASE64Encoder();
-            base64 = encoder.encode(signature.sign());
-        } catch (FileNotFoundException e) {
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            return Base64.encodeBase64String(signature.sign());
+        } catch (KeyStoreException | CertificateException | UnrecoverableKeyException
+                | NoSuchAlgorithmException | IOException | SignatureException | InvalidKeyException ex) {
+            throw new IllegalStateException("can not load cert from repository.");
         }
-        System.out.println("test = " + base64);
-        return base64;
     }
 
     public boolean enCodeByCer(String val, String msg) {
         boolean flag = false;
         try {
-            String file = Pkipair.class.getResource("99bill[1].cert.rsa.20140803.cer").toURI().getPath();
-            System.out.println(file);
+            String file = PkiPairUtil.class.getResource("99bill[1].cert.rsa.20140803.cer").toURI().getPath();
             FileInputStream inStream = new FileInputStream(file);
-
             CertificateFactory cf = CertificateFactory.getInstance("X.509");
             X509Certificate cert = (X509Certificate) cf.generateCertificate(inStream);
             PublicKey pk = cert.getPublicKey();
@@ -52,7 +44,6 @@ public class Pkipair {
             sun.misc.BASE64Decoder decoder = new sun.misc.BASE64Decoder();
             System.out.println(new String(decoder.decodeBuffer(msg)));
             flag = signature.verify(decoder.decodeBuffer(msg));
-            System.out.println(flag);
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("no");
