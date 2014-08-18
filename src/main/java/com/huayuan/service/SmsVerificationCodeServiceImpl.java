a74a90b6d95b3db6e9b5f077a1065c424e808941
tusc_heqiao@163.com <heqiao75518?>
2014-08-18 16:17:45 +0800

    1.Update the sms message provider for sending message using the new cloud provider.,

--- src/main/java/com/huayuan/service/SmsVerificationCodeServiceImpl.java
+++ src/main/java/com/huayuan/service/SmsVerificationCodeServiceImpl.java
@@ -1,21 +1,11 @@
 package com.huayuan.service;
 
+import com.cloopen.rest.sdk.CCPRestSDK;
 import org.apache.commons.lang.RandomStringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 
-import javax.inject.Inject;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.text.MessageFormat;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -24,41 +14,29 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service(value = "smsVerificationCodeService")
 @Transactional
 public class SmsVerificationCodeServiceImpl implements SmsVerificationCodeService {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SmsVerificationCodeService.class);
-    private static final String SEND_SUCCESS = "<State>0</State>";
+    private static final String SEND_SUCCESS = "000000";
     private static final ConcurrentHashMap<String, String> MOBILE_PHONE_VERIFICATION_CODE_MAP = new ConcurrentHashMap<>();
-    @Inject
-    private RestTemplate restTemplate;
-    @Value("${sms.template}")
-    private String smsTemplate;
-    @Value("${sms.postTemplate}")
-    private String postTemplate;
-    @Value("${sms.url}")
-    private String smsUrl;
-
-    private String getSmsMessage(String mobilePhone, String code) {
-        try {
-            String msg = URLEncoder.encode(MessageFormat.format(smsTemplate, code), "UTF-8");
-            return MessageFormat.format(postTemplate, mobilePhone, msg);
-        } catch (UnsupportedEncodingException e) {
-            LOGGER.error("sms message can not be encoded.");
-        }
-        return null;
-    }
+    @Value("${sms.cloud.host}")
+    private String smsHost;
+    @Value("${sms.cloud.port}")
+    private String smsPort;
+    @Value("${sms.cloud.account}")
+    private String smsAccount;
+    @Value("${sms.cloud.token}")
+    private String smsToken;
+    @Value("${sms.cloud.appid}")
+    private String smsAppId;
+    @Value("${sms.cloud.template}")
+    private String smsTemplateId;
 
     @Override
     public boolean sendVerificationCode(String mobilePhone) {
         String code = RandomStringUtils.randomNumeric(6);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        HttpEntity req = new HttpEntity<>(getSmsMessage(mobilePhone, code), headers);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity response = restTemplate.postForEntity(smsUrl, req, String.class);
-        if (response.toString().contains(SEND_SUCCESS)) {
-            MOBILE_PHONE_VERIFICATION_CODE_MAP.put(mobilePhone, code);
-            return true;
-        }
-        return false;
+        CCPRestSDK restAPI = new CCPRestSDK();
+        restAPI.init(smsHost, smsPort);
+        restAPI.setAccount(smsAccount, smsToken);
+        restAPI.setAppId(smsAppId);
+        return SEND_SUCCESS.equals(restAPI.sendTemplateSMS(mobilePhone, smsTemplateId, new String[]{code}).get("statusCode"));
     }
 
     @Override
