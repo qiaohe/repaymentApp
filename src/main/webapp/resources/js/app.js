61dc43ceaf231d9e63dfc8ede1e3d3d4c5748e83
Thomas-Peng <debianthomas@gmail.com>
2014-08-26 11:28:48 +0800

    fix the number with 2 decimals before redirection

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1633,7 +1633,8 @@ if(!(/pay-success/.test(window.location) || /pay-fail/.test(window.location))) {
             }
 
             $(".repay-item-pay").tap(function() {
-                var orderAmount = member.loan.loans[member.crntCaro].curDueAmt;
+//                var orderAmount = member.loan.loans[member.crntCaro].curDueAmt;
+                var orderAmount = member.loan.loans[member.crntCaro].curDueAmt.toFixed(2);
                 window.location = "http://godzilla.dlinkddns.com.cn/repaymentApp/" + config.apiPath + "account/repay/" + member.id + "/" + member.loan.loans[member.crntCaro].loanId + "/" + orderAmount;
             });
         });
