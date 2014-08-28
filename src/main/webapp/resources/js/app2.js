66c9e0bd27d2d5ec9ccb37d4176e41eb2683ec3b
Richard.Xue <liming.snowy@gmail.com>
2014-08-28 16:48:42 +0800

    Fix the pic of share team page

--- src/main/webapp/resources/js/app2.js
+++ src/main/webapp/resources/js/app2.js
@@ -37,4 +37,51 @@ $(document).on("pagecreate", "#team", function () {
     $(".member-item-right").width(curWidth*0.9-85);
 });
 
+WeixinApi.ready(function(Api) {
+    // 微信分享的数据
+    var wxData = {
+        "appId": "", // 服务号可以填写appId
+        "imgUrl" : 'http://wechat.memedai.cn/repaymentApp/resources/img/public/r120.png',
+        "link" : window.location.href,
+        "desc" : $.mobile.activePage.attr("data-title"),
+        "title" : $.mobile.activePage.attr("data-title")
+    };
+
+    // 分享的回调
+    var wxCallbacks = {
+        // 分享操作开始之前
+        ready : function() {
+            // 你可以在这里对分享的数据进行重组
+            console.log("准备分享");
+        },
+        // 分享被用户自动取消
+        cancel : function(resp) {
+            // 你可以在你的页面上给用户一个小Tip，为什么要取消呢？
+            console.log("分享被取消");
+        },
+        // 分享失败了
+        fail : function(resp) {
+            // 分享失败了，是不是可以告诉用户：不要紧，可能是网络问题，一会儿再试试？
+            console.log("分享失败");
+        },
+        // 分享成功
+        confirm : function(resp) {
+            // 分享成功了，我们是不是可以做一些分享统计呢？
+            //window.location.href='http://192.168.1.128:8080/wwyj/test.html';
+            console.log("分享成功");
+        },
+        // 整个分享过程结束
+        all : function(resp) {
+            // 如果你做的是一个鼓励用户进行分享的产品，在这里是不是可以给用户一些反馈了？
+            console.log("分享结束");
+        }
+    };
+    // 用户点开右上角popup菜单后，点击分享给好友，会执行下面这个代码
+    Api.shareToFriend(wxData, wxCallbacks);
+    // 点击分享到朋友圈，会执行下面这个代码
+    Api.shareToTimeline(wxData, wxCallbacks);
+    // 点击分享到腾讯微博，会执行下面这个代码
+    Api.shareToWeibo(wxData, wxCallbacks);
+});
+
 console.log("END!");
